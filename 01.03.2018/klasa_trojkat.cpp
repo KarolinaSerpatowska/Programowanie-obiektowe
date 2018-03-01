115c4b9e52a02cfa9351ef8a0b8a4725358603b4
Vitaliy Mysak <15235025+Donaim@users.noreply.github.com>
2018-03-01 17:15:35 +0100

    Update klasa_trojkat.cpp
    completed trojkąt
    to do: 
    move declarations to .h

--- 01.03.2018/klasa_trojkat.cpp
+++ 01.03.2018/klasa_trojkat.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
+#include <cmath> // for distance
 using namespace std;
+
+#include "point.cpp" // definicje
+
+float distance(point a, point b) { sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2)); }
 
 class trojkat
 {
+    point a;
+    point b;
+    point c;
 public:
-	int a;
-	int b;
-	int c;
+    Triangle(point a, point b, point c) {
+	    this->a = a;
+	    this->b = b;
+	    this->c = c;
+    }
+	
+    float obw() { return distance(a, b) + distance(b, c) + distance(c, a); } 
+    float area() { 
+		float p = obw() / 2;
+		return sqrt(p 
+		* (p - distance(a, b)) 
+		* (p - distance(b, c))
+		* (p - distance(c, a)));
+	}
 };
 
 
 int main()
 {
-	trojkat x;
-	int obw;
-	cin >> x.a;
-	cin >> x.b;
-	cin >> x.c;
-	obw = x.a + x.b + x.c;
+	trojkat x (point(2,3), point(5,7), point(11,13));
+	float obw;
+	obw = x.obw();
 	cout << obw;
 	cin.get();
 	cin.get();
