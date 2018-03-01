c053fdf59e3eeaf8a2f38d9ebbe2ca0e19df6fca
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2018-03-01 14:24:59 +0100

    Create klasa_trojkat.cpp

---
+++ 01.03.2018/klasa_trojkat.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class trojkat
+{
+public:
+	int a;
+	int b;
+	int c;
+};
+
+
+int main()
+{
+	trojkat x;
+	int obw;
+	x.a = 1;
+	x.b = 2;
+	x.c = 3;
+	obw = x.a + x.b + x.c;
+
+    return 0;
+}
