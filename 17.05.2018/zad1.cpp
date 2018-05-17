0427ee8a7f7e3c6de2c8479ea0b4d715728aabb6
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2018-05-17 15:04:51 +0200

    Create zad1.cpp

---
+++ 17.05.2018/zad1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+template <class Typ>
+Typ &funkcja(Typ *t1, Typ *t2)
+{
+	Typ najmniejszy=*t1;
+	for (Typ *i = t1; i !=t2 ; i++)
+	{
+		if (najmniejszy > *i)
+		{
+			najmniejszy = *i;
+		}
+	}
+	return najmniejszy;
+}
+
+int main()
+{
+	int tab[3];
+	tab[0] = 1;
+	tab[1] = 0;
+	tab[2] = 4;
+	cout << funkcja<int>(&tab[0], &tab[2]) << endl;
+
+	system("PAUSE");
+    return 0;
+}
