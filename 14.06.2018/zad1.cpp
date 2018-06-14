97c92da6f1c19beea2e51b265a5d57b5d2a1c611
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2018-06-14 15:08:41 +0200

    Create zad1.cpp

---
+++ 14.06.2018/zad1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void clear(int a, int b, vector<int>&z)
+{
+	replace_if(z.begin(), z.end(), [a,b](int z)->bool {return !(z >= a && z <= b); }, 0);
+}
+
+int main()
+{
+	vector<int> v;
+	v.push_back(5);
+	v.push_back(1);
+	v.push_back(3);
+	v.push_back(7);
+	sort(v.begin(), v.end());
+	for (int i = 0; i <(int) v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+	
+	clear(10, 12, v);
+
+	for (int i = 0; i <(int) v.size(); i++)
+	{
+	cout << "po funkcji" << v[i] << endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}
